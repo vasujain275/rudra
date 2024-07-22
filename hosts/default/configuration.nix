c2316100d25ac25e0b85a99fa73e756558679836
vasujain275 <vasujain275@gmail.com>
2024-07-22 07:14:18 +0530

    Gen 10: moved to wall based colors

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -87,24 +87,24 @@
     # Styling Options
   stylix = {
     enable = true;
-    base16Scheme = {
-      base00 = "191724";
-      base01 = "1f1d2e";
-      base02 = "26233a";
-      base03 = "6e6a86";
-      base04 = "908caa";
-      base05 = "e0def4";
-      base06 = "e0def4";
-      base07 = "524f67";
-      base08 = "eb6f92";
-      base09 = "f6c177";
-      base0A = "ebbcba";
-      base0B = "31748f";
-      base0C = "9ccfd8";
-      base0D = "c4a7e7";
-      base0E = "f6c177";
-      base0F = "524f67";
-    };
+    # base16Scheme = {
+    #   base00 = "191724";
+    #   base01 = "1f1d2e";
+    #   base02 = "26233a";
+    #   base03 = "6e6a86";
+    #   base04 = "908caa";
+    #   base05 = "e0def4";
+    #   base06 = "e0def4";
+    #   base07 = "524f67";
+    #   base08 = "eb6f92";
+    #   base09 = "f6c177";
+    #   base0A = "ebbcba";
+    #   base0B = "31748f";
+    #   base0C = "9ccfd8";
+    #   base0D = "c4a7e7";
+    #   base0E = "f6c177";
+    #   base0F = "524f67";
+    # };
     image = /home/vasu/.cache/wall.png;
     polarity = "dark";
     opacity.terminal = 0.8;
