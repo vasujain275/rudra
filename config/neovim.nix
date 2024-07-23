6822e8cb008c04e7c485520f153382270aaa2d37
vasujain275 <vasujain275@gmail.com>
2024-07-23 23:51:22 +0530

    Fix nvim

--- config/neovim.nix
+++ config/neovim.nix
@@ -1,9 +1,7 @@
 { pkgs, inputs, ... }:
 let
-  finecmdline = pkgs.vimUtils.buildVimPlugin {
-    name = "fine-cmdline";
-    src = inputs.fine-cmdline;
-  };
+  toLua = str: "lua << EOF\n${str}\nEOF\n";
+  toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
 in
 {
   programs = {
@@ -17,24 +15,40 @@ in
       extraPackages = with pkgs; [
         lua-language-server
         gopls
-        xclip
         wl-clipboard
         luajitPackages.lua-lsp
         nil
         rust-analyzer
-        #nodePackages.bash-language-server
         yaml-language-server
         pyright
         marksman
       ];
       plugins = with pkgs.vimPlugins; [
+
+        nvim-cmp 
+        {
+          plugin = nvim-cmp;
+          config = toLuaFile ./nvim/plugin/autocomplete.lua;
+        }
+        cmp_luasnip
+        cmp-nvim-lsp
+        luasnip
+        cmp-path
+        friendly-snippets        
+
+        {
+          plugin = conform-nvim;
+          config = toLuaFile ./nvim/plugin/autoformat.lua;
+        }
+
+
+
         alpha-nvim
         auto-session
         bufferline-nvim
         dressing-nvim
         indent-blankline-nvim
         nui-nvim
-        finecmdline
         nvim-treesitter.withAllGrammars
         lualine-nvim
         nvim-autopairs
@@ -64,24 +78,25 @@ in
         nnoremap : <cmd>FineCmdline<CR>
       '';
       extraLuaConfig = ''
-        ${builtins.readFile ./nvim/options.lua}
-        ${builtins.readFile ./nvim/keymaps.lua}
-        ${builtins.readFile ./nvim/plugins/alpha.lua}
+        ${builtins.readFile ./nvim/plugins/autocomplete.lua}
+        ${builtins.readFile ./nvim/plugins/autoformat.lua}
         ${builtins.readFile ./nvim/plugins/autopairs.lua}
-        ${builtins.readFile ./nvim/plugins/auto-session.lua}
-        ${builtins.readFile ./nvim/plugins/comment.lua}
-        ${builtins.readFile ./nvim/plugins/cmp.lua}
+        ${builtins.readFile ./nvim/plugins/color-scheme.lua}
+        ${builtins.readFile ./nvim/plugins/dashboard.lua}
+        ${builtins.readFile ./nvim/plugins/formatting.lua}
+        ${builtins.readFile ./nvim/plugins/git-signs.lua}
+        ${builtins.readFile ./nvim/plugins/indent_line.lua}
+        ${builtins.readFile ./nvim/plugins/init.lua}
+        ${builtins.readFile ./nvim/plugins/lazygit.lua}
+        ${builtins.readFile ./nvim/plugins/linting.lua}
         ${builtins.readFile ./nvim/plugins/lsp.lua}
-        ${builtins.readFile ./nvim/plugins/nvim-tree.lua}
+        ${builtins.readFile ./nvim/plugins/mini-nvim.lua}
+        ${builtins.readFile ./nvim/plugins/oil.lua}
+        ${builtins.readFile ./nvim/plugins/rename.lua}
+        ${builtins.readFile ./nvim/plugins/rust.lua}
         ${builtins.readFile ./nvim/plugins/telescope.lua}
-        ${builtins.readFile ./nvim/plugins/todo-comments.lua}
         ${builtins.readFile ./nvim/plugins/treesitter.lua}
-        ${builtins.readFile ./nvim/plugins/fine-cmdline.lua}
-        require("ibl").setup()
-        require("bufferline").setup{}
-        require("lualine").setup({
-          icons_enabled = true,
-        })
+        ${builtins.readFile ./nvim/plugins/which-key.lua}
       '';
     };
   };
--- config/nvim/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-require('nvim-treesitter.configs').setup {
-    ensure_installed = {},
-
-    auto_install = false,
-
-    highlight = { enable = true },
-
-    indent = { enable = true },
-}
