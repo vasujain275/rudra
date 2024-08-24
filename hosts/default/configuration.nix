a7ce95d32f254902756dd85c4aa63988729cf3a0
vasujain275 <vasujain275@gmail.com>
2024-08-25 01:34:42 +0530

    added waybar starship

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -141,75 +141,6 @@
     #   package = pkgs.nix-ld-rs;
     # };
     firefox.enable = false;
-    starship = {
-      enable = true;
-      settings = {
-        add_newline = false;
-        buf = {
-          symbol = " ";
-        };
-        c = {
-          symbol = " ";
-        };
-        directory = {
-          read_only = " 󰌾";
-        };
-        docker_context = {
-          symbol = " ";
-        };
-        fossil_branch = {
-          symbol = " ";
-        };
-        git_branch = {
-          symbol = " ";
-        };
-        golang = {
-          symbol = " ";
-        };
-        hg_branch = {
-          symbol = " ";
-        };
-        hostname = {
-          ssh_symbol = " ";
-        };
-        lua = {
-          symbol = " ";
-        };
-        memory_usage = {
-          symbol = "󰍛 ";
-        };
-        meson = {
-          symbol = "󰔷 ";
-        };
-        nim = {
-          symbol = "󰆥 ";
-        };
-        nix_shell = {
-          symbol = " ";
-        };
-        nodejs = {
-          symbol = " ";
-        };
-        ocaml = {
-          symbol = " ";
-        };
-        package = {
-          symbol = "󰏗 ";
-        };
-        python = {
-          symbol = " ";
-        };
-        rust = {
-          symbol = " ";
-        };
-        swift = {
-          symbol = " ";
-        };
-        zig = {
-          symbol = " ";
-        };
-      };
-    };
     dconf.enable = true;
     #seahorse.enable = true;
     fuse.userAllowOther = true;
@@ -251,7 +182,7 @@
     wget
     killall
     eza
-    #starship
+    starship
     # oh-my-posh
     imagemagick
     zoxide
@@ -352,7 +283,7 @@
     swww
     hyprlock
     waypaper
-    #waybar
+    waybar
     dunst
     ninja
     brightnessctl
