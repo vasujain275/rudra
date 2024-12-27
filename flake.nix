70dd996a3edd55ca2a20ff31759c2f1e76a4fffa
vasujain275 <vasujain275@gmail.com>
2024-12-27 13:48:04 +0530

    Updated Zen Browser Flake URL

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       url = "path:/home/vasu/rudra/modules/nixCats";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
     ghostty = {
       url = "github:ghostty-org/ghostty";
     };
