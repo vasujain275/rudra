2b9087fc4add7909416565885825b9e9d2a7ca53
vasujain275 <vasujain275@gmail.com>
2024-12-28 00:19:46 +0530

    Switched to Zen as Default Browser

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -224,6 +224,7 @@ in {
     lazygit
     lazydocker
     bruno
+    bruno-cli
     gnumake
     coreutils
     nixfmt-rfc-style
@@ -559,15 +560,15 @@ in {
 
   xdg.mime.defaultApplications = {
     # Web and HTML
-    "x-scheme-handler/http" = "firefox.desktop";
-    "x-scheme-handler/https" = "firefox.desktop";
-    "x-scheme-handler/chrome" = "firefox.desktop";
-    "text/html" = "firefox.desktop";
-    "application/x-extension-htm" = "firefox.desktop";
-    "application/x-extension-html" = "firefox.desktop";
-    "application/x-extension-shtml" = "firefox.desktop";
-    "application/x-extension-xhtml" = "firefox.desktop";
-    "application/xhtml+xml" = "firefox.desktop";
+    "x-scheme-handler/http" = "zen.desktop";
+    "x-scheme-handler/https" = "zen.desktop";
+    "x-scheme-handler/chrome" = "zen.desktop";
+    "text/html" = "zen.desktop";
+    "application/x-extension-htm" = "zen.desktop";
+    "application/x-extension-html" = "zen.desktop";
+    "application/x-extension-shtml" = "zen.desktop";
+    "application/x-extension-xhtml" = "zen.desktop";
+    "application/xhtml+xml" = "zen.desktop";
 
     # File management
     "inode/directory" = "org.kde.dolphin.desktop";
@@ -594,15 +595,15 @@ in {
     "application/vnd.openxmlformats-officedocument.presentationml.presentation" = "libreoffice-impress.desktop";
 
     # PDF
-    "application/pdf" = "firefox.desktop";
+    "application/pdf" = "zen.desktop";
 
     # Torrents
     "application/x-bittorrent" = "org.qbittorrent.qBittorrent.desktop";
     "x-scheme-handler/magnet" = "org.qbittorrent.qBittorrent.desktop";
 
     # Other handlers
-    "x-scheme-handler/about" = "firefox.desktop";
-    "x-scheme-handler/unknown" = "firefox.desktop";
+    "x-scheme-handler/about" = "zen.desktop";
+    "x-scheme-handler/unknown" = "zen.desktop";
     "x-scheme-handler/postman" = "Postman.desktop";
     "x-scheme-handler/tonsite" = "org.telegram.desktop.desktop";
   };
